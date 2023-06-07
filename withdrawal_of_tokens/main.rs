3fb137bf85085dd4944df16525012fc1324999f4
antonovmike <antonovma@outlook.com>
2023-06-07 17:00:50 +0500

    removed extra code

--- withdrawal_of_tokens/main.rs
+++ withdrawal_of_tokens/main.rs
@@ -1,18 +1,19 @@
 use reqwest::Client;
 // use serde_json::Value;
 
+const RECIPIENT_ADDRESS_1: &str = "recipient_address_1";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = dotenv::var("OKCOIN_API_KEY").expect("OKCOIN_API_KEY not found");
     let api_secret = dotenv::var("OKCOIN_API_SECRET").expect("OKCOIN_API_SECRET not found");
-    let recipient = dotenv::var("RECIPIENT_ADDRESS_1").expect("RECIPIENT_ADDRESS_1 not found");
 
     let amount = "1000";
 
     let client = Client::new();
     let url = format!(
         "https://www.okcoin.com/api/v5/private/transaction/withdraw?api_key={}&api_secret={}&currency=STX&amount={}&address={}", 
-        api_key, api_secret, amount, recipient
+        api_key, api_secret, amount, RECIPIENT_ADDRESS_1
     );
 
     // let response: Value = client.get(&url).send().await?.json().await?;
