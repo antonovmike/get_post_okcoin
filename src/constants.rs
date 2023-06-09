5cc7f077ea5fdfadedbd05fd2206dbebbf5ad2cf
antonovmike <antonovma@outlook.com>
2023-06-09 12:42:12 +0500

    constants.rs

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+pub const AMOUNT: u64 = 0;
+
+pub const URL_BASE: &str = "https://www.okcoin.com";
+pub const URL_BALANCE: &str = "/api/v5/account/balance?ccy=STX";
+pub const URL_WITHDRAWAL: &str = "/api/v5/asset/withdrawal?ccy=STX";
+
+pub const RECIPIENT_ADDR_1: &str = "RECIPIENT_ADDRESS_1";
+pub const RECIPIENT_ADDR_2: &str = "RECIPIENT_ADDRESS_2";
