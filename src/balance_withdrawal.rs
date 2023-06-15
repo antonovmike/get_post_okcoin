77813b25ccda3fecb97da96b5ddb2422ff91c7ad
antonovmike <antonovma@outlook.com>
2023-06-15 16:41:27 +0500

    lifetimes issue

--- src/balance_withdrawal.rs
+++ src/balance_withdrawal.rs
@@ -5,7 +5,7 @@ use async_trait::async_trait;
 use base64::engine::{general_purpose, Engine};
 use hmac_sha256::HMAC;
 use reqwest::Client;
-// use serde_json::json;
+use serde_json::json;
 // use tokio::time::Timeout;
 
 use crate::constants::*;
@@ -91,8 +91,41 @@ pub trait ExchangeClient {
 
         Ok(current_balance)
     }
-    fn withdraw(&self, _address: String) -> Result<(), Box<dyn Error>> {
-        todo!()
+    async fn withdraw(&self, current_balance: f64, address: String) -> Result<(), Box<dyn Error>> {
+        let key_and_pass = personal_data().await;
+        let client = Client::new();
+    
+        let timestamp = humantime::format_rfc3339_millis(std::time::SystemTime::now());
+    
+        let body = json!({
+            "amt": current_balance,
+            "fee":"0.0005",
+            "dest":"3",
+            "ccy":"BTC",
+            "chain":"BTC-Bitcoin",
+            "toAddr": address
+        });
+    
+        let message = format!("{timestamp}POST{URL_WITHDRAWAL}{body}");
+        let sign = general_purpose::STANDARD.encode(HMAC::mac(message, &key_and_pass[1]));
+    
+        let request = client
+            .post(format!("{URL_BASE}{URL_WITHDRAWAL}"))
+            .header("accept", "application/json")
+            .header("CONTENT-TYPE", "application/json")
+            .header("OK-ACCESS-KEY", &key_and_pass[0])
+            .header("OK-ACCESS-SIGN", sign)
+            .header("OK-ACCESS-TIMESTAMP", format!("{timestamp}"))
+            .header("OK-ACCESS-PASSPHRASE", &key_and_pass[2])
+            .body(body.to_string())
+            .build()?;
+    
+        let response = client.execute(request).await?;
+    
+        let json = response.text().await?;
+        println!("POST: {}", &json);
+    
+        Ok(())
     }
 }
 
