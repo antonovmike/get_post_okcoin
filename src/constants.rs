759fdb703321b7e9d52c94b639ad9b0d5d2c3300
antonovmike <antonovma@outlook.com>
2023-06-16 20:19:57 +0500

    timeout

--- src/constants.rs
+++ src/constants.rs
@@ -9,7 +9,6 @@ pub const URL_WITHDRAWAL: &str = "/api/v5/asset/withdrawal";
 
 // test fake address
 pub const RECIPIENT_ADDR_1: &str = "17DKe3kkkkiiiiTvAKKi2vMPbm1Bz3CMKw";
-#[allow(unused)]
 pub const RECIPIENT_ADDR_2: &str = "17DKe3kkkkiiiiTvAKKi2vMPbm1Bz3CMKw";
 
-pub const TIMEOUT: u64 = 3;
+pub const TIMEOUT: u64 = 3; // Timeout in seconds
