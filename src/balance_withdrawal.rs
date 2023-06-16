20e628f445c9059e5b079203f8fda706236f8bc1
antonovmike <antonovma@outlook.com>
2023-06-16 16:03:42 +0500

    fmt

--- src/balance_withdrawal.rs
+++ src/balance_withdrawal.rs
@@ -50,25 +50,6 @@ impl<EC: ExchangeClient + std::marker::Sync> Service<EC> {
     }
 }
 
-async fn personal_data() -> Vec<String> {
-    let api_key = dotenv::var("OKCOIN_API_KEY").expect("OKCOIN_API_KEY not found");
-    let api_secret = dotenv::var("OKCOIN_API_SECRET").expect("OKCOIN_API_SECRET not found");
-    let passphrase = dotenv::var("OKCOIN_PASS_PHRASE").expect("OKCOIN_PASS_PHRASE not found");
-
-    let api_and_pass = vec![api_key, api_secret, passphrase];
-    api_and_pass
-}
-
-#[async_trait]
-pub trait ExchangeClient {
-    async fn get_balance(&self) -> Result<f64, Box<dyn Error>> {
-        Ok(270.0) // fake balance
-    }
-    async fn withdraw(&self, _current_balance: f64, _address: String) -> Result<(), Box<dyn Error>> {
-        Ok(())
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct OkCoinClient {
     pub api_key: String,
@@ -85,6 +66,16 @@ impl OkCoinClient {
             base_url,
             secret,
         }
+    }
+}
+
+#[async_trait]
+pub trait ExchangeClient {
+    async fn get_balance(&self) -> Result<f64, Box<dyn Error>> {
+        Ok(270.0) // fake balance
+    }
+    async fn withdraw(&self, _current_balance: f64, _address: String) -> Result<(), Box<dyn Error>> {
+        Ok(())
     }
 }
 
@@ -160,6 +151,15 @@ impl ExchangeClient for OkCoinClient {
     
         Ok(())
     }
+}
+
+async fn personal_data() -> Vec<String> {
+    let api_key = dotenv::var("OKCOIN_API_KEY").expect("OKCOIN_API_KEY not found");
+    let api_secret = dotenv::var("OKCOIN_API_SECRET").expect("OKCOIN_API_SECRET not found");
+    let passphrase = dotenv::var("OKCOIN_PASS_PHRASE").expect("OKCOIN_PASS_PHRASE not found");
+
+    let api_and_pass = vec![api_key, api_secret, passphrase];
+    api_and_pass
 }
 
 #[cfg(test)]
